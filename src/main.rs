4f61993b4f19227d3c1795d726fc0c0ae0fe0c4c
Philipp Armingeon <philipp.armingeon@aiven.io>
2022-09-19 13:13:24 -0500

    updated main.rs to reflect changes made to library

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ const NCHANNELS: u16 = 1;
 const NSAMPLES: u32 = NCHANNELS as u32 * DURATION * SR;
 
 fn main() {
+    // Let's first define the required metadata which will go into the header of our WAV file 
     let config = Config {
         sample_size: SAMPLE_SIZE,
         channels: NCHANNELS,
@@ -20,7 +21,18 @@ fn main() {
     let frequency = 220.0;
     let amplitude = 0.2; 
 
-    let mut wav_file = Wave::new(&config);    
-    wav_file.write("output-3.wav", frequency, amplitude);
+    // We can then use the config to initialise a new WAV file
+    let mut wav = Wave::new(&config);    
+
+    // Let's now create a sine using the frequency and amplitude we defined above  
+    wav.write_sine("sine.wav", frequency, amplitude);
+
+    // We can also create a WAVE variable by reading it from a file
+    // NOTE: At this point this also requires us to pass in a config which we define ourselves. 
+    // Removing this necessity is a task for a future refactor
+    let _wav = read("sine.wav", &config); 
+    
+    // We can now read the associated sample data directly from the struct field
+    wav.read_data();
 }
 
